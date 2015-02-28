53c8e72f020102af2e71e480df12b88696cbcc77
MaximeHelaine <mhelainedu91@live.fr>
2015-02-28 16:16:21 +0100

    Ajout des modèle et du ssystème de spawwn Random
    + Une partie du système d'animation

---
+++ BattleArmy/Assets/Script/ArmyFollow.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmyFollow : MonoBehaviour {
+
+
+    public ArmyController controller;
+    public Animator animatorController;
+
+    Vector3 GetSeparationVector(Transform target)
+    {
+        var diff = transform.position - target.transform.position;
+        var diffLen = diff.magnitude;
+        var scaler = Mathf.Clamp01(1.0f - diffLen / controller.neighborDist);
+        return diff * (scaler / diffLen);
+    }
+
+    void Update()
+    {
+        animatorController.SetFloat("Velocity", controller.velocity);
+        var currentPosition = transform.position;
+        var currentRotation = transform.rotation;
+
+        // Calcule la velocity avec une coefficiant random.
+        var randomCoef = Random.Range(0, 1);
+        var velocity = controller.velocity * (1.0f + randomCoef * controller.velocityVariation);
+
+        // Initialisation des vecteurs correspondantes aux 3 règles.
+        var separation = Vector3.zero;
+        var alignment = controller.transform.forward;
+        var cohesion = controller.transform.position;
+
+        // Récupérations des boids voisins.
+        var neighborsBoids = Physics.OverlapSphere(currentPosition, controller.neighborDist, controller.searchLayer);
+
+        // Calcule de la "separation", "l'alignement" et de la "cohésion" moyenne de touts les boids.
+        foreach (var boid in neighborsBoids)
+        {
+            if (boid.gameObject == gameObject) continue; // Si le gameObject correspond au boids actuel, on continue.
+            var t = boid.transform;
+            separation += GetSeparationVector(t);
+            alignment += t.forward;
+            cohesion += t.position;
+        }
+
+        //Division par le nombdre de boids afin de récupérer l'algnement et la cohesion
+        var average = 1.0f / neighborsBoids.Length;
+        alignment *= average;
+        cohesion *= average;
+        cohesion = (cohesion - currentPosition).normalized;
+
+        // Calcule de la direction du boids
+        var direction_tmp = separation + alignment + cohesion;
+        var direction = new Vector3(direction_tmp.x, 0, direction_tmp.z);//separation + alignment + cohesion;
+        var rotation = Quaternion.FromToRotation(Vector3.forward, direction.normalized);
+
+        // Smooth du changement de direction
+        if (rotation != currentRotation)
+        {
+            Quaternion rotation_tmp = Quaternion.Slerp(rotation, currentRotation, 0.8f);
+            transform.rotation = new Quaternion(0, rotation_tmp.y, 0, rotation_tmp.w);//Quaternion.Slerp(rotation, currentRotation, 0.8f);
+           
+        }
+            
+
+        // Déplacement du boids.
+        Vector3 transform_tmp = currentPosition + transform.forward * (velocity * Time.deltaTime);
+        transform.position = new Vector3(transform_tmp.x, transform.position.y, transform_tmp.z);//currentPosition + transform.forward * (velocity * Time.deltaTime);
+    }
+}
---
+++ BattleArmy/Assets/Script/BoidController.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class BoidController : MonoBehaviour {
+
+    private List<GameObject> boidsArray = new List<GameObject>();
+    
+    public GameObject boidPrefab;
+
+    public Text[] slidersText = new Text[4];
+    public Slider[] slider = new Slider[4];
+
+    public int spawnCount = 10;
+
+    public float spawnRadius = 4.0f;
+
+    [Range(0.1f, 20.0f)]
+    public float velocity = 6.0f;
+
+    [Range(0.0f, 0.9f)]
+    public float velocityVariation = 0.5f;
+
+    [Range(0.1f, 10.0f)]
+    public float neighborDist = 2.0f;
+
+    public LayerMask searchLayer;
+
+    void Start()
+    {
+
+        slidersText[0].text = spawnCount.ToString();
+        slidersText[1].text = spawnRadius.ToString();
+        slidersText[2].text = velocity.ToString();
+        slidersText[3].text = neighborDist.ToString();
+
+        slider[0].value = spawnCount;
+        slider[1].value = spawnRadius;
+        slider[2].value = velocity;
+        slider[3].value = neighborDist;
+
+        for (var i = 0; i < spawnCount; i++) Spawn();
+    }
+
+    public GameObject Spawn()
+    {
+        return Spawn(transform.position + Random.insideUnitSphere * spawnRadius);
+    }
+
+    public GameObject Spawn(Vector3 position)
+    {
+        var rotation = Quaternion.Slerp(transform.rotation, Random.rotation, 0.3f);
+        var boid = Instantiate(boidPrefab, position, rotation) as GameObject;
+        boid.GetComponent<BoidsFollow>().controller = this;
+        boidsArray.Add(boid);
+        return boid;
+    }
+
+    public void setSpawnCountAndLabel()
+    {
+        spawnCount = (int)slider[0].value;
+        slidersText[0].text = slider[0].value.ToString();
+    }
+
+    public void setSpawnRadiusAndLabel()
+    {
+        spawnRadius = slider[1].value;
+        slidersText[1].text = slider[1].value.ToString();
+    }
+
+    public void setVelocityAndLabel()
+    {
+        velocity = slider[2].value;
+        slidersText[2].text = slider[2].value.ToString();
+    }
+
+    public void setNeighborDistAndLabel()
+    {
+        neighborDist = slider[3].value; 
+        slidersText[3].text = slider[3].value.ToString();
+    }
+
+    public void resetBoids()
+    {
+        for (int i = 0; i < boidsArray.Count; i++)
+        {
+            Destroy(boidsArray[i]);
+        }
+        boidsArray = new List<GameObject>();
+        for (var i = 0; i < spawnCount; i++) Spawn();
+    }
+
+    public void leaveGame()
+    {
+        Application.Quit();
+    }
+}
