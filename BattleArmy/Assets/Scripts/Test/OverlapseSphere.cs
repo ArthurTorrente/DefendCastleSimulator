1f541f868dcafaca057256bd4af64f26b9c6dff0
ArthurTorrente <torrente.arthur@gmail.com>
2015-03-05 23:37:53 +0100

    no message

---
+++ BattleArmy/Assets/Scripts/Test/OverlapseSphere.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverlapseSphere : MonoBehaviour {
+
+    [SerializeField]
+    private LayerMask m_layer;
+
+	void Start ()
+    {
+        if (Physics.OverlapSphere(transform.position, 1, m_layer).Length > 0)
+            Debug.Log("Have collide");
+        else
+            Debug.Log("NO !!");
+	}
+}
