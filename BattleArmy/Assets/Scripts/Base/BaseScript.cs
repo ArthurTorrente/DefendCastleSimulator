69f98896d4ce3fab57c91655e351672ec674d1b1
AlexandreRivet <alex-rivet94@hotmail.fr>
2015-03-06 00:49:25 +0100

    Début ajout pdv à la base
    - Ajout d'un script de pdv (pas fini -> gérer le takeDamage et dans le
    getVision la priorité sur les ennemis)

---
+++ BattleArmy/Assets/Scripts/Base/BaseScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseScript : MonoBehaviour 
+{
+    [SerializeField]
+    private Transform m_transform;
+    public Transform Transform
+    {
+        get { return m_transform; }
+        set { m_transform = value; }
+    }
+
+    [SerializeField]
+    private HealthManager m_healthManager;
+    public HealthManager HealthManager
+    {
+        get { return m_healthManager; }
+        set { m_healthManager = value; }
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
